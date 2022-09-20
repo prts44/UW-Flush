1a4f5d2a69cbf36f097b4594ae36ec68f92378b4
z <ryan.ckit57@gmail.com>
2022-09-19 20:52:19 -0400

    Implement basic route generation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-    return (<h1>hello</h1>);
+
+    const [routes, setRoutes] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/locations')
+            .then((res) => res.json())
+            .then((data) => setRoutes(data.map((br) => <Route path={"/" + br.id} element={<div>{br.name}</div>}/>)));
+    }, []);
+
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<h1>hello</h1>}/>
+                {routes}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
